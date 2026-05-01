e3345fd62a75943ede5972d19178a1fb3fc1b1cf
leadsandbrand <anitesh123.na@gmail.com>
2026-05-01 17:49:02 +0530

    origin

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: "http://127.0.0.1:5500"
+    origin: "http://127.0.0.1:5500",
+     origin:  "https://abhinavhomes.in",
  }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
